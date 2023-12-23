fa53aa7131c2ed7dab7b66e48258b4cbed9182ff
szabokristof <kristof.szabo@logmein.com>
2023-12-23 15:30:19 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿var ArrayWriter = (string output, int maxNumber) =>
+ Array.ForEach(Enumerable.Repeat(output, Random.Shared.Next(2, maxNumber)).ToArray(), Console.WriteLine);
+
+if (!int.TryParse(args.Length is 0 ? string.Empty : args[0], out int result))
+    ArrayWriter("Ez zselés?!", 42);
+else
+    ArrayWriter("Ez zselés?!", result);
